aa6adfdde54777c842e508308cbc765b150cb9a1
Peter Bittner <peter.bittner@vshn.ch>
2019-11-07 16:20:26 +0100

    Also test against Python 3.8

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     python_requires='>=3.6',
     install_requires=read_file('requirements.in'),
